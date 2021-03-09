7a3bcd8f53528cf6df33eef2524049a684e78143
Maxemelianum <76476734+Maxemelianum@users.noreply.github.com>
2021-03-09 14:32:10 +0300

    bot_v1.2

--- Botovsi_main.py
+++ Botovsi_main.py
@@ -1,9 +1,10 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 import random
-import telebot
 
-TOKEN = '1591047164:AAHZ_9l-rKzWnosUc8nAaj3vtd-xgIN3_MI'
+
+TOKEN = '1591047164:AAGPm_L7jDwhNds_vMlEiCfiw0b6xY1s5zU'
+
 
 what = [
     'Идиот, я же не понимаю нихрена!',
@@ -48,10 +49,12 @@ def main():
     start_handler = CommandHandler('start', do_start)
     stop_handler = CommandHandler('stop', do_stop)
     name = CommandHandler('user', do_user)
+    some = MessageHandler(Filters.text, do_some)
     sticker = CommandHandler('sticker', do_text)
 
     dispather.add_handler(start_handler)
     dispather.add_handler(handler)
+    dispather.add_handler(some)
     dispather.add_handler(stop_handler)
     dispather.add_handler(sticker)
     dispather.add_handler(name)
@@ -66,11 +69,16 @@ def do_echo(update, context):
 
 
 def do_start(update, context):
-    update.message.reply_text(randomizer.choice(hi))
+
+    update.message.reply_text(randomizer.choice(hi)
+)
 
 
 def do_bulling(update, context):
-    update.message.reply_text(randomizer.choice(helper))
+    keyboard = [['1', '2', '3'],
+                ['4', '5', '6']]
+    update.message.reply_text(randomizer.choice(helper),
+    reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True))
 
 
 def do_stop(update, context):
@@ -87,6 +95,18 @@ def do_text(update, context):
         img = 'https://imbt.ga/dBmlYLNaCQ'
         update.message.reply_text(randomizer.choice(stick))
         update.message.reply_text(f'{img}')
+
+
+def do_some(update: Update, context):
+    text = update.message.text
+    if text == '1':
+        update.message.reply_text('Это 1', reply_markup=ReplyKeyboardRemove())
+    elif text == '2':
+        update.message.reply_text('Это 2', reply_markup=ReplyKeyboardRemove())
+    elif text == '3':
+        update.message.reply_text('Это 3', reply_markup=ReplyKeyboardRemove())
+    else:
+        update.message.reply_text(randomizer.choice(what), reply_markup=ReplyKeyboardRemove())
 
 
 main()
