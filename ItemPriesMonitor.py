b57b640f2a6e0c803ba5c4a3a00c8ad95094998d
Maxemelianum <76476734+Maxemelianum@users.noreply.github.com>
2021-04-21 14:15:32 +0300

    item_bot_v0.2beta

--- ItemPriesMonitor.py
+++ ItemPriesMonitor.py
@@ -9,9 +9,12 @@ def main():
     updater = Updater(token=TOKEN)  # объект, который ловит сообщения от Telegrama
 
     dispather = updater.dispatcher
+
+    start_help = CommandHandler('help', do_help)
     start_handler = CommandHandler('start', do_start)
     echo = MessageHandler(Filters.all, do_echo)
 
+    dispather.add_handler(start_help)
     dispather.add_handler(start_handler)
     dispather.add_handler(echo)
 
@@ -25,6 +28,28 @@ def do_echo(update, context):
 
 def do_start(update, context):
     update.message.reply_text('Привет!')
+
+
+def do_help(update, context):
+    keyboard = [['1'], ['1']]
+    update.message.reply_text('jr'),
+    ReplyKeyboardMarkup(keyboard)
+
+
+def do_some(update: Update, context):
+    text = update.message.text
+
+    if text == 'Нет':
+        update.message.reply_text('****** ответ', reply_markup=ReplyKeyboardRemove())
+        update.message.reply_sticker('CAACAgIAAxkBAAECEd1gUd5kmaGDSLkdJEidOZwxCp7C_AACGAADoF_dLYLQt9fbwjB_HgQ')
+    elif text == 'Да':
+        update.message.reply_text('Вы использовали команду /user', reply_markup=ReplyKeyboardRemove())
+    elif text == 'Давай не будем':
+        update.message.reply_text('Вы использовали команду /stop', reply_markup=ReplyKeyboardRemove())
+    elif text == 'Хватит':
+        update.message.reply_text('Вы использовали команду /img', reply_markup=ReplyKeyboardRemove())
+    else:
+        update.message.reply_text('чаво', reply_markup=ReplyKeyboardRemove())
 
 
 main()
