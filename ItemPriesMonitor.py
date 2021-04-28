e92806b1fd6f021dcbc865f8ef4fb10a2cc27b64
Maxemelianum <76476734+Maxemelianum@users.noreply.github.com>
2021-04-28 14:10:34 +0300

    item_bot_v0.4beta

--- ItemPriesMonitor.py
+++ ItemPriesMonitor.py
@@ -12,10 +12,12 @@ def main():
 
     start_help = CommandHandler('help', do_help)
     start_handler = CommandHandler('start', do_start)
+    some_handler =  MessageHandler(Filters.text, do_some)
     echo = MessageHandler(Filters.all, do_echo)
 
     dispather.add_handler(start_help)
     dispather.add_handler(start_handler)
+    dispather.add_handler(some_handler)
     dispather.add_handler(echo)
 
     updater.start_polling()
@@ -31,22 +33,24 @@ def do_start(update, context):
 
 
 def do_help(update, context):
-    keyboard = [['1'], ['1']]
+    keyboard = [['Поиск Items'], ['Список недавно просмотренных Items'], ['Отслеживание стоимости Items'],
+                ['Список ваших Items']]
     reply_markup = ReplyKeyboardMarkup(keyboard=keyboard, one_time_keyboard=True, resize_keyboard=True)
-    update.message.reply_text('jr', reply_markup=reply_markup)
+    update.message.reply_text('Это раздел HELP, который поможет вам при работе с ботом.', reply_markup=reply_markup)
 
 
 def do_some(update: Update, context):
     text = update.message.text
 
-    if text == 'Нет':
+    if text == 'Поиск Items':
         update.message.reply_text('****** ответ', reply_markup=ReplyKeyboardRemove())
         update.message.reply_sticker('CAACAgIAAxkBAAECEd1gUd5kmaGDSLkdJEidOZwxCp7C_AACGAADoF_dLYLQt9fbwjB_HgQ')
-    elif text == 'Да':
-        update.message.reply_text('Вы использовали команду /user', reply_markup=ReplyKeyboardRemove())
-    elif text == 'Давай не будем':
+    elif text == 'Список недавно просмотренных Items':
+        update.message.reply_text('Вы использовали команду /user'
+                                  '\nспасибо', reply_markup=ReplyKeyboardRemove())
+    elif text == 'Отслеживание стоимости Items':
         update.message.reply_text('Вы использовали команду /stop', reply_markup=ReplyKeyboardRemove())
-    elif text == 'Хватит':
+    elif text == 'Список ваших Items':
         update.message.reply_text('Вы использовали команду /img', reply_markup=ReplyKeyboardRemove())
     else:
         update.message.reply_text('чаво', reply_markup=ReplyKeyboardRemove())
